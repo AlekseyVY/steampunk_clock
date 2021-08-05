c49349e997d4bff89bcc4411265225c11f0b5434
AlekseyVY <alekseyvy@gmail.com>
2021-08-05 13:57:09 +0300

    fix

---
+++ js/main.js
@@ -0,0 +1,26 @@
+setInterval(clock, 1000);
+
+const hours = document.querySelector('[data-hour]');
+const minutes = document.querySelector('[data-minute]');
+
+const secondsCog = document.querySelector('[data-secondsCog]');
+const minuteCog = document.querySelector('[data-minuteCog]');
+const hourCog = document.querySelector('[data-hourCog]')
+
+function clock() {
+    const date = new Date();
+    const secondsDeg = date.getSeconds() / 60;
+    const minutesDeg = (secondsDeg + date.getMinutes()) / 60;
+    const hoursDeg = (minutesDeg + date.getHours()) / 12;
+    animate(hours, hoursDeg);
+    animate(minutes, minutesDeg)
+    animate(hourCog, hoursDeg)
+    animate(minuteCog, minutesDeg)
+    animate(secondsCog, secondsDeg)
+}
+
+function animate(ele, deg) {
+    ele.style.setProperty('--rotation', deg * 360)
+}
+
+clock();
